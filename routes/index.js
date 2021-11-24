a5b94304744202def6fd4b105aeb792a7a6eca60
Esteban-Duarte31 <yessidduarte7@gmail.com>
2021-11-24 18:12:15 -0500

    portularse a un empleo (Funcional)

--- routes/index.js
+++ routes/index.js
@@ -1,65 +1,80 @@
-const express= require('express');
+const express = require('express');
 const router = express.Router();
 
 const colombia = require('./../resources/files/colombia');
-let num=0;
+let num = 0;
+let numWork;
 let students = []
 
-router.get('/',(req,res)=>{
-    res.render("index",{students:students,title:"Página de Inicio"});
+router.get('/', (req, res) => {
+    res.render("index", { students: students, title: "Página de Inicio" });
 });
 
-router.get('/insert',(req, res)=>{
-    res.render('insert',{title:"Agregar Trabajo",
-        departments:colombia.departments,
-        towns:colombia.towns});
+router.get('/insert', (req, res) => {
+    res.render('insert', {
+        title: "Agregar Trabajo",
+        departments: colombia.departments,
+        towns: colombia.towns
+    });
 });
 
-router.post('/insert',(req,res)=>{
-    const{cargo, name, salary, gender, dpto, town, email, phone, comments } = req.body;
-    const dptoAux = colombia.departments.find( record => record.code == dpto ).name;
-    const townAux = colombia.towns.find( record => record.code == town ).name;
-    const city = townAux.concat( '-', dptoAux );
-    const genAux = gender == 'F' ? "Femenino" : gender == 'M' ? "Masculino": "No importa";
-    let candidates=[];
+router.post('/insert', (req, res) => {
+    const { cargo, name, salary, gender, dpto, town, email, phone, comments } = req.body;
+    const dptoAux = colombia.departments.find(record => record.code == dpto).name;
+    const townAux = colombia.towns.find(record => record.code == town).name;
+    const city = townAux.concat('-', dptoAux);
+    const genAux = gender == 'F' ? "Femenino" : gender == 'M' ? "Masculino" : "No importa";
+    let candidates = [];
     num++;
-    let newReg = {num,cargo, name, salary, genAux, city, email, phone, comments, candidates };
+    let newReg = { num, cargo, name, salary, genAux, city, email, phone, comments, candidates };
     students.push(newReg);
     res.redirect('/');
 });
 
 router.get('/deleteWork/:num', (req, res) => {
     const num = req.params.num;
-    students = students.filter(function(work) {
-        return work.num != num; 
+    students = students.filter(function (work) {
+        return work.num != num;
     });
     res.redirect('/');
-    
+
 });
 
 
 
 // INSERTAR PERSONA A UN TRABAJO
-router.get('/insertPerson',(req, res)=>{
-    res.render('insertPerson',{title:"Agregar Persona",
-        departments:colombia.departments,
-        towns:colombia.towns});
+router.get('/insertPerson/:num', (req, res) => {
+    numWork= req.params.num;
+    res.render('insertPerson', {
+        title: "Agregar Persona",
+        departments: colombia.departments,
+        towns: colombia.towns
+    });
 });
 
-router.post('/insertPerson',(req,res)=>{
-    const{idPerson, name, lastName, gender, dpto, town, address, email, phone, professionalProfile } = req.body;
-    const dptoAux = colombia.departments.find( record => record.code == dpto ).name;
-    const townAux = colombia.towns.find( record => record.code == town ).name;
-    const names = name.concat(' ', lastName);
-    const city = townAux.concat( '-', dptoAux );
-    const genAux = gender == 'F' ? "Femenino" : gender == 'M' ? "Masculino": "Otro";
-    let newReg = {idPerson, names, genAux, city, address, email, phone, professionalProfile  };
-    students.push(newReg);
-    res.redirect('/');
+router.post('/insertPerson/:num', (req, res) => {
+
+
+    students.forEach(function (work, index, object) {
+        console.log("aqui");
+        if (work.num == numWork) {
+            const { idPerson, name, lastName, gender, dpto, town, address, email, phone, professionalProfile } = req.body;
+            const dptoAux = colombia.departments.find(record => record.code == dpto).name;
+            const townAux = colombia.towns.find(record => record.code == town).name;
+            const names = name.concat(' ', lastName);
+            const city = townAux.concat('-', dptoAux);
+            const genAux = gender == 'F' ? "Femenino" : gender == 'M' ? "Masculino" : "Otro";
+            let newReg = { idPerson, names, genAux, city, address, email, phone, professionalProfile };
+            object[index].candidates.push(newReg);
+            res.redirect('/');
+        }
+    });
+
+
 });
 
-router.get('/about',(req,res)=>{
-   res.render('about',{title:"Sobre Nosotros"});
+router.get('/about', (req, res) => {
+    res.render('about', { title: "Sobre Nosotros" });
 });
 
 module.exports = router;
