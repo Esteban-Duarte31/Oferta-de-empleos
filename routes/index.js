41866fef0d0e66ad62407af007b3ab73dfdf906e
Esteban-Duarte31 <yessidduarte7@gmail.com>
2021-11-24 20:53:12 -0500

    interfaz para visualizar todos los candidatos

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const colombia = require('./../resources/files/colombia');
 let num = 0;
 let numWork;
-let students = []
+let works = []
 
 router.get('/', (req, res) => {
-    res.render("index", { students: students, title: "Página de Inicio" });
+    res.render("index", { works: works, title: "Página de Inicio" });
 });
 
 router.get('/insert', (req, res) => {
@@ -27,13 +27,13 @@ router.post('/insert', (req, res) => {
     let candidates = [];
     num++;
     let newReg = { num, cargo, name, salary, genAux, city, email, phone, comments, candidates };
-    students.push(newReg);
+    works.push(newReg);
     res.redirect('/');
 });
 
 router.get('/deleteWork/:num', (req, res) => {
     const num = req.params.num;
-    students = students.filter(function (work) {
+    works = works.filter(function (work) {
         return work.num != num;
     });
     res.redirect('/');
@@ -44,7 +44,7 @@ router.get('/deleteWork/:num', (req, res) => {
 
 // INSERTAR PERSONA A UN TRABAJO
 router.get('/insertPerson/:num', (req, res) => {
-    numWork= req.params.num;
+    numWork = req.params.num;
     res.render('insertPerson', {
         title: "Agregar Persona",
         departments: colombia.departments,
@@ -53,10 +53,7 @@ router.get('/insertPerson/:num', (req, res) => {
 });
 
 router.post('/insertPerson/:num', (req, res) => {
-
-
-    students.forEach(function (work, index, object) {
-        console.log("aqui");
+    works.forEach(function (work, index, object) {
         if (work.num == numWork) {
             const { idPerson, name, lastName, gender, dpto, town, address, email, phone, professionalProfile } = req.body;
             const dptoAux = colombia.departments.find(record => record.code == dpto).name;
@@ -72,6 +69,30 @@ router.post('/insertPerson/:num', (req, res) => {
 
 
 });
+
+
+// CANDIDATOS
+router.get('/candidates', (req, res) => {
+    res.render('candidates', {
+        works: works, 
+        title: "Total Candidatos"
+    });
+});
+
+// router.post('/insert', (req, res) => {
+//     const { cargo, name, salary, gender, dpto, town, email, phone, comments } = req.body;
+//     const dptoAux = colombia.departments.find(record => record.code == dpto).name;
+//     const townAux = colombia.towns.find(record => record.code == town).name;
+//     const city = townAux.concat('-', dptoAux);
+//     const genAux = gender == 'F' ? "Femenino" : gender == 'M' ? "Masculino" : "No importa";
+//     let candidates = [];
+//     num++;
+//     let newReg = { num, cargo, name, salary, genAux, city, email, phone, comments, candidates };
+//     works.push(newReg);
+//     res.redirect('/');
+// });
+
+
 
 router.get('/about', (req, res) => {
     res.render('about', { title: "Sobre Nosotros" });
